0aceabe49a3378b5fa8e13630a3f71b1f13b7240
wtildestar <31348327+wtildestar@users.noreply.github.com>
2019-12-23 22:50:08 +0500

    Initial Commit

---
+++ VKfeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKfeed
+//
+//  Created by wtildestar on 23/12/2019.
+//  Copyright © 2019 wtildestar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
